6948ea9c17c30f269b2fb8a30e0eb2522d93fd2f
York <york4818@gmail.com>
2025-12-16 16:16:03 +0800

    新增 repo

---
+++ src/main/java/com/ntou/auctionSite/repository/MessageRepository.java
@@ -0,0 +1,13 @@
+package com.ntou.auctionSite.repository;
+
+import com.ntou.auctionSite.model.Message;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MessageRepository extends MongoRepository<Message, String> {
+    List<Message> findByChatId(String chatId);
+}
+
